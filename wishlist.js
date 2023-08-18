495664966655d4cb791981d87917460c52232547
milyssaSidisaid <moimelly451@gmail.com>
2023-08-18 22:36:14 +0100

    Button

---
+++ wishlist.js
@@ -0,0 +1,10 @@
+let addlist=document.getElementById('addlist');
+let toDocontainet=document.getElementById('todoContainer');
+let inpuField=document.getElementById('inputField');
+
+addlist.addEventListener('click',function(){
+    let list=document.createElement('list');
+    list.innerText=inputField.value;
+
+    toDocontainet.appendChild(list)
+})
